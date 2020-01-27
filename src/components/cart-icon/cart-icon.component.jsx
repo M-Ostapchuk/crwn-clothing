3a2075b4e8803e7c587d6b703fa38eaca472aac1
unknown <you@example.com>
2020-01-27 23:28:33 +0200

    added cartIcon and cart-dropdown component to our application, also implemented cart reducer

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './cart-icon.styles.scss';
+import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+import {connect} from 'react-redux';
+import {tooggleCartHidden} from '../../redux/cart/cart.actions';
+
+const CartIcon = ({tooggleCartHidden}) => {
+    return (
+    <div className="cart-icon" onClick={tooggleCartHidden}>
+        <ShoppingIcon className="shopping-icon"/>
+        <span className="item-count">0</span>
+    </div>)
+}
+
+const mapDispatchToProps = dispatch => ({
+    tooggleCartHidden: () => dispatch(tooggleCartHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,5 @@
+import cartActionTypes from "./cart.types";
+
+export const tooggleCartHidden = () => ({
+    type: cartActionTypes.TOGGLE_CART_HIDDEN
+})
