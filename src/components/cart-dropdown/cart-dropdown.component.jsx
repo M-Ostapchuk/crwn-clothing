b8c2290c8902ddb20015da48cfd8848560000824
unknown <you@example.com>
2020-02-01 00:44:01 +0200

    updating our application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
-import CartItem from '../cart-item/cart-item.component'
+import CartItem from '../cart-item/cart-item.component';
+import {selectCartItems} from '../../redux/cart/cart.selectors'
 
 import {connect} from 'react-redux';
 
@@ -18,8 +19,8 @@ const CartDropdown = ({cartItems}) => {
     )
 }
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps, null)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,12 +3,13 @@ import './cart-icon.styles.scss';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {connect} from 'react-redux';
 import {tooggleCartHidden} from '../../redux/cart/cart.actions';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
 
-const CartIcon = ({tooggleCartHidden}) => {
+const CartIcon = ({tooggleCartHidden, itemCount}) => {
     return (
     <div className="cart-icon" onClick={tooggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
     </div>)
 }
 
@@ -16,4 +17,9 @@ const mapDispatchToProps = dispatch => ({
     tooggleCartHidden: () => dispatch(tooggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
